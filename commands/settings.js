ef283a5306a9e41da2d9b1159f22f30a63be3251
Aditya Gupta <adityagupta4213@gmail.com>
2017-09-25 19:32:48 +0530

    Added settings

--- commands/settings.js
+++ commands/settings.js
@@ -7,6 +7,15 @@ Object.keys(_colors).forEach(function (key) {
   colors[key] = parseInt(value)
 })
 
+function convertChannelIdToName(message, channel) {
+  let _channel = message.guild.channels.find('id', channel)
+  return ` ${_channel}`
+}
+
+function convertBooltoEmoji(value) {
+  return value == 'true' ? `:white_check_mark:` : `:x:`
+}
+
 exports.run = (bot, message, args) => {
   const settings = JSON.parse(fs.readFileSync(`./data/${message.guild.id}.json`, 'utf8'))
   message.channel.send({
@@ -19,10 +28,30 @@ exports.run = (bot, message, args) => {
       author: {
         name: 'SETTINGS'
       },
-      fields: [
-        {
-          'name': 
-        }
+      fields: [{
+          'name': 'Prefix',
+          'value': `${settings.prefix}`
+        }, {
+          'name': 'Server Log',
+          'value': `${convertBooltoEmoji(settings.serverlog)}`,
+          'inline': true
+        }, {
+          'name': 'Member Log',
+          'value': `${convertBooltoEmoji(settings.memberlog)}`,
+          'inline': true
+        }, {
+          'name': 'Welcome Channel',
+          'value': `${message.guild.channels.find('name', settings.welcomechannel)}`
+        }, {
+          'name': 'Filter Invites',
+          'value': `${settings.filterinvites.map(channel=>convertChannelIdToName(message, channel))}`,
+          'inline': true
+        }, {
+          'name': 'Filter Links',
+          'value': `${settings.filterlinks.map(channel=>convertChannelIdToName(message, channel))}`,
+          'inline': true
+        },
+
       ]
     }
   })

60f06f476d1dd28c6537e8833b9e0398fb8756d4
Aditya Gupta <adityagupta4213@gmail.com>
2017-10-08 22:03:14 +0530

    Added command to view user avatar

---
+++ commands/avatar.js
@@ -0,0 +1,26 @@
+const config = require('../config.json')
+const _colors = require('../colors.json')
+// Change string values to int from colors.json
+const colors = {}
+Object.keys(_colors).forEach(function (key) {
+  let value = _colors[key]
+  colors[key] = parseInt(value)
+})
+
+exports.run = (bot, message, args) => {
+  const user = message.mentions.users.first()
+  if (!user) return message.reply('Mention a user to get the avatar from')
+  if (!user.avatarURL) user.avatarURL = user.defaultAvatarURL
+  message.channel.send({
+    embed: {
+      color: colors.blue,
+      author: {
+        name: user.username
+      },
+      description: `Avatar URL: ${user.avatarURL}`,
+      image: {
+        url: user.avatarURL
+      }
+    }
+  })
+}
